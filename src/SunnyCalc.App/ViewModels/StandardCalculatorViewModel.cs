c879871be2295872c9002eaa48286b97c4baa6bc
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2020-04-06 17:19:31 +0200

    Add basic calculator GUI
    This commit implements the StandardCalculatorView user control, which is used
    in the basic calculator mode that doesn't offer expressions nor goniometric
    functions.
    
    There's a custom control called DoubleButton, which encapsulates two buttons
    into a single control with no apparent visual division. It's used for packing
    together pairs of operations, which is done to make the calculator look more
    compact.
    
    The StandardCalculatorViewModel class will be used as the main functional entity
    of this view and will be implemented in following revisions.
    
    I've chosen the Roboto font, which is now included in the app's resources. This
    may or may not be subject to further discussion. Also, a hopefully temporary
    app icon has been added.

---
+++ src/SunnyCalc.App/ViewModels/StandardCalculatorViewModel.cs
@@ -0,0 +1,88 @@
+﻿using ReactiveUI;
+using SunnyCalc.Maths;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+using System.Windows.Input;
+
+namespace SunnyCalc.App.ViewModels
+{
+    public class StandardCalculatorViewModel : ViewModelBase
+    {
+        private enum Operation
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide,
+            NRoot,
+            NPow,
+            None
+        }
+
+        public ICommand NumberCommand { get; }
+        public ICommand OperationCommand { get; }
+        public ICommand DecimalPointCommand { get; }
+
+        /// <summary>
+        /// The text to show on the calculator "display".
+        /// </summary>
+        public string CurrentState
+        {
+            get => _curState;
+            set => this.RaiseAndSetIfChanged(ref _curState, value);
+        }
+
+        /// <summary>
+        /// Controls whether the decimal point button can be used, in context of the currently displayed number.
+        /// </summary>
+        public bool DecimalPointEnabled
+        {
+            get => _canUseDecimal;
+            set => this.RaiseAndSetIfChanged(ref _canUseDecimal, value);
+        }
+
+        private Operation _nextOp;
+        private string _curState = "0";
+        private bool _canUseDecimal = true;
+        private decimal _lastResult = 0;
+        private IMathsService _serv;
+
+        public StandardCalculatorViewModel(IMathsService mathsService)
+        {
+            _serv = mathsService;
+
+            this.NumberCommand = ReactiveCommand.Create<string>(PerformNumberCommand);
+            this.OperationCommand = ReactiveCommand.Create<string>(PerformOperationCommand);
+            this.DecimalPointCommand =
+                ReactiveCommand.Create(AddDecimalPoint, this.WhenAnyValue(x => x.DecimalPointEnabled));
+        }
+
+        private void UpdateState(decimal newNumber)
+        {
+        }
+
+        private void UpdateLastResult()
+        {
+            
+        }
+        
+        public void PerformOperationCommand(string op)
+        {
+
+            this.DecimalPointEnabled = true;
+        }
+
+        public void PerformNumberCommand(string number)
+        {
+            this.CurrentState = number;
+        }
+
+        public void AddDecimalPoint()
+        {
+            this.DecimalPointEnabled = false;
+            this.CurrentState += ".";
+        }
+    }
+}
---
+++ src/SunnyCalc.App/Views/StandardCalculatorView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using SunnyCalc.App.ViewModels;
+using System;
+using System.Linq;
+
+namespace SunnyCalc.App.Views
+{
+    public class StandardCalculatorView : UserControl
+    {
+        public StandardCalculatorView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        protected override void OnDataContextChanged(EventArgs e)
+        {
+            base.OnDataContextChanged(e);
+            
+            // The numpad buttons have to be generated after the data context is bound,
+            // so that their commands can be assigned to the viewmodel.
+            this.InitNumpad();
+        }
+
+        /// <summary>
+        /// Generates the 1 to 9 buttons and adds them to the "numpad" grid.
+        /// </summary>
+        private void InitNumpad()
+        {
+            var numpad = this.FindControl<Grid>("numpad");
+
+            // This should never happen, as the data context will never get changed more than once,
+            // but as a precaution, remove the number buttons that might be present in the grid.
+            var currentBtns = numpad.Children.Where(
+                c => c.Name?.StartsWith("numberBtn_") ?? false).ToList();
+
+            if (currentBtns.Count > 0)
+            {
+                numpad.Children.RemoveAll(currentBtns);
+            }
+
+            for (var i = 0; i < 9; i++)
+            {
+                var numBtn = new Button {Name = "numberBtn_" + i};
+
+                numBtn.Classes.Add("numBtn");
+                numBtn.Content = (i + 1).ToString();
+                numpad.Children.Add(numBtn);
+
+                numBtn.Command = (this.DataContext as StandardCalculatorViewModel)?.NumberCommand;
+                numBtn.CommandParameter = numBtn.Content;
+
+                Grid.SetColumn(numBtn, i % 3);
+                Grid.SetRow(numBtn, 2 - (i / 3));
+            }
+        }
+    }
+}
