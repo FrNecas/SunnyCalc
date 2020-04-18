3c70a0cca0dc3ab34e139d1fc2185aa84abbfd3a
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2020-04-18 16:53:39 +0200

    Add view switching

--- src/SunnyCalc.App/ViewModels/MainWindowViewModel.cs
+++ src/SunnyCalc.App/ViewModels/MainWindowViewModel.cs
@@ -11,17 +11,28 @@ namespace SunnyCalc.App.ViewModels
         private readonly IMathsService _service;
 
         private StandardCalculatorViewModel _cvm;
+
         public StandardCalculatorViewModel StandardCalculatorViewModel
         {
             get => _cvm;
             set => this.RaiseAndSetIfChanged(ref _cvm, value);
         }
-        
+
         private ExtendedCalculatorViewModel _evm;
+
         public ExtendedCalculatorViewModel ExtendedCalculatorViewModel
         {
             get => _evm;
             set => this.RaiseAndSetIfChanged(ref _evm, value);
+        }
+
+
+        private ViewModelBase _currentVm;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentVm;
+            private set => this.RaiseAndSetIfChanged(ref _currentVm, value);
         }
 
         public MainWindowViewModel()
@@ -31,8 +42,15 @@ namespace SunnyCalc.App.ViewModels
         public MainWindowViewModel(IMathsService mathsService)
         {
             _service = mathsService;
+
             this.StandardCalculatorViewModel = new StandardCalculatorViewModel(mathsService);
             this.ExtendedCalculatorViewModel = new ExtendedCalculatorViewModel(mathsService);
+            this.CurrentViewModel = this.StandardCalculatorViewModel;
+        }
+
+        public void SwapViews()
+        {
+            this.CurrentViewModel = _currentVm == _cvm ? (ViewModelBase) _evm : _cvm;
         }
     }
 }
